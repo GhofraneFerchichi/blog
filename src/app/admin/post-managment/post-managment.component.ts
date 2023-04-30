b5f612520c00b06753b2a7293059a47989313f0b
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2023-04-30 20:17:51 +0100

    update post management

--- src/app/admin/post-managment/post-managment.component.ts
+++ src/app/admin/post-managment/post-managment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostServiceService } from 'src/app/services/post-service.service';
 
 @Component({
   selector: 'app-post-managment',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-managment.component.css']
 })
 export class PostManagmentComponent implements OnInit {
-
-  constructor() { }
+  listofpost:Post[];
+  constructor(private ps:PostServiceService) { }
 
   ngOnInit(): void {
+    this.getallpost();
   }
-
+getallpost(){
+  this.ps.getPost().subscribe(
+    data=>{
+      console.log(data)
+      this.listofpost=data;
+    }
+  )
+}
+supprimer(post :any){
+  this.ps.deletePost(post.postId).subscribe(()=>this.ps.getPost().subscribe(
+    data=>{
+      this.listofpost=data
+      
+    }
+  )
+  );
+}
 }
